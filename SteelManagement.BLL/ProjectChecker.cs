963b737164f0d8930343aeb6b8de9ef023474b6e
YXP <954222619@qq.com>
2018-04-09 16:23:12 +0800

    添加ProjectChecker三层文件

---
+++ SteelManagement.BLL/ProjectChecker.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using SteelManagement.Model;
+namespace SteelManagement.BLL
+{
+	/// <summary>
+	/// ProjectChecker
+	/// </summary>
+	public partial class ProjectChecker
+	{
+		private readonly SteelManagement.DAL.ProjectChecker dal=new SteelManagement.DAL.ProjectChecker();
+		public ProjectChecker()
+		{}
+		#region  BasicMethod
+
+		/// <summary>
+		/// 得到最大ID
+		/// </summary>
+		public int GetMaxId()
+		{
+			return dal.GetMaxId();
+		}
+
+		/// <summary>
+		/// 是否存在该记录
+		/// </summary>
+		public bool Exists(int Id)
+		{
+			return dal.Exists(Id);
+		}
+
+		/// <summary>
+		/// 增加一条数据
+		/// </summary>
+		public int  Add(SteelManagement.Model.ProjectChecker model)
+		{
+			return dal.Add(model);
+		}
+
+		/// <summary>
+		/// 更新一条数据
+		/// </summary>
+		public bool Update(SteelManagement.Model.ProjectChecker model)
+		{
+			return dal.Update(model);
+		}
+
+		/// <summary>
+		/// 删除一条数据
+		/// </summary>
+		public bool Delete(int Id)
+		{
+			
+			return dal.Delete(Id);
+		}
+
+
+		/// <summary>
+		/// 得到一个对象实体
+		/// </summary>
+		public SteelManagement.Model.ProjectChecker GetModel(int Id)
+		{
+			
+			return dal.GetModel(Id);
+		}
+
+
+
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public DataSet GetList(string strWhere)
+		{
+			return dal.GetList(strWhere);
+		}
+		/// <summary>
+		/// 获得前几行数据
+		/// </summary>
+		public DataSet GetList(int Top,string strWhere,string filedOrder)
+		{
+			return dal.GetList(Top,strWhere,filedOrder);
+		}
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public List<SteelManagement.Model.ProjectChecker> GetModelList(string strWhere)
+		{
+			DataSet ds = dal.GetList(strWhere);
+			return DataTableToList(ds.Tables[0]);
+		}
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public List<SteelManagement.Model.ProjectChecker> DataTableToList(DataTable dt)
+		{
+			List<SteelManagement.Model.ProjectChecker> modelList = new List<SteelManagement.Model.ProjectChecker>();
+			int rowsCount = dt.Rows.Count;
+			if (rowsCount > 0)
+			{
+				SteelManagement.Model.ProjectChecker model;
+				for (int n = 0; n < rowsCount; n++)
+				{
+					model = dal.DataRowToModel(dt.Rows[n]);
+					if (model != null)
+					{
+						modelList.Add(model);
+					}
+				}
+			}
+			return modelList;
+		}
+
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public DataSet GetAllList()
+		{
+			return GetList("");
+		}
+
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		public int GetRecordCount(string strWhere)
+		{
+			return dal.GetRecordCount(strWhere);
+		}
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		public DataSet GetListByPage(string strWhere, string orderby, int startIndex, int endIndex)
+		{
+			return dal.GetListByPage( strWhere,  orderby,  startIndex,  endIndex);
+		}
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		//public DataSet GetList(int PageSize,int PageIndex,string strWhere)
+		//{
+			//return dal.GetList(PageSize,PageIndex,strWhere);
+		//}
+
+		#endregion  BasicMethod
+		#region  ExtensionMethod
+
+		#endregion  ExtensionMethod
+	}
+}
+
---
+++ SteelManagement.Model/ProjectChecker.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace SteelManagement.Model
+{
+	/// <summary>
+	/// ProjectChecker:实体类(属性说明自动提取数据库字段的描述信息)
+	/// </summary>
+	[Serializable]
+	public partial class ProjectChecker
+	{
+		public ProjectChecker()
+		{}
+		#region Model
+		private int _id;
+		private DateTime? _entrytime;
+		private string _project;
+		private string _username;
+		private int? _userid;
+		/// <summary>
+		/// 
+		/// </summary>
+		public int Id
+		{
+			set{ _id=value;}
+			get{return _id;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public DateTime? EntryTime
+		{
+			set{ _entrytime=value;}
+			get{return _entrytime;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Project
+		{
+			set{ _project=value;}
+			get{return _project;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public string UserName
+		{
+			set{ _username=value;}
+			get{return _username;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public int? UserId
+		{
+			set{ _userid=value;}
+			get{return _userid;}
+		}
+		#endregion Model
+
+	}
+}
+
